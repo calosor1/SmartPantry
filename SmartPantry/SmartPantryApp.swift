4c5ec62f347c7e30290fa64c68677280e9f5b82e
calosor1 <228862714+calosor1@users.noreply.github.com>
2026-09-22 18:56:04 -0400

    Initial Commit

---
+++ SmartPantry/SmartPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPantryApp.swift
+//  SmartPantry
+//
+//  Created by Alexandre on 2026-09-22.
+//
+
+import SwiftUI
+
+@main
+struct SmartPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
